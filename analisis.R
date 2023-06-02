9fb07323512f03e03cb0e164548e6fc74a358c59
brunobian <brunobian@gmail.com>
2023-06-02 15:55:39 -0300

    sigo analizando

--- analisis.R
+++ analisis.R
@@ -29,19 +29,87 @@ read.pcibex <- function(filepath, auto.colnames=TRUE, fun.col=function(col,cols)
   }
 }
 library("dplyr")
+library("ggplot2")
+library("maditr")
 
-setwd("/home/bbianchi/Downloads/PCIbex/")
+
+setwd("/data/brunobian/Documents/Repos/Repos_Analisis/polisemia/")
 results <- read.pcibex("results_1_648.csv")
+results$Value <- as.character(results$Value)
+results$PennElementName <- as.character(results$PennElementName)
 
-rtas = results[results$PennElementName=="opciones",]
+rtas = data.frame()
+for (i in 1:dim(results)[1]){
+  thisLine = results[i,]
+  if (thisLine[,"PennElementName"] == "mail" & thisLine[,"Value"] != ""){
+    mail =  results[i,"Value"]
+  }
+  thisLine$mail = mail
+  if (results[i,"PennElementName"] == "opciones"){
+    rtas =  rbind(rtas,thisLine)
+  }
+}
+
+
+# rtas = results[results$PennElementName=="opciones",]
+
+rtas$IDOrac <- as.numeric(as.character(rtas$IDOrac))
+rtas$indTarget = rtas$IDOrac %% 100
+rtas$contextType = rtas$IDOrac %/% 100
 
 rtas$acc <- (rtas$Value == rtas$SignEsperado) | (rtas$Value == paste0(rtas$SignEsperado,2))
-rtas <- rtas[,c("IDOrac","SignEsperado","Target","acc")]
+rtas <- rtas[,c("IDOrac","SignEsperado","Target","acc","indTarget","contextType","mail")]
 
-group_by(rtas,"IDOrac")
+# Check acc suj: miro sujs que tienen baja acc en Contexto 1 y 2 (los sesgadores)
+porSuj <- rtas[rtas$contextType<3,] %>%  group_by(mail) %>% 
+            summarise( n = n(), 
+                       mean = mean(acc)
+            )
+filtSujAcc = porSuj[porSuj[porSuj$n>0,]$mean < .2,]$mail
 
-agrupadas <- rtas %>%  group_by(IDOrac) %>% 
-             summarise(mean = mean(acc), n = n())
+# Check catch suj: miro sujs que hicieron catch trials (tipo 4)
+porSuj <- rtas[rtas$contextType==4,] %>%  group_by(mail) %>% 
+  summarise( n = n())
+filtSujCatch = setdiff(unique(rtas$mail),porSuj$mail)
+
+filtSujs = unique(c(filtSujCatch,filtSujAcc))
+
+rtas$filtSuj <- is.element(rtas$mail, filtSujs)
+
+rtasFilt <- rtas[rtas$filtSuj==FALSE,]
+
+###
+
+agrupadas <- rtasFilt %>%  group_by(IDOrac) %>% 
+             summarise(mean = mean(acc), 
+                       n = n(),
+                       indTarget = mean(indTarget),
+                       contextType = mean(contextType))
 
 plot(agrupadas$IDOrac,agrupadas$mean)
 plot(agrupadas$IDOrac,agrupadas$n)
+
+porContext <- agrupadas[agrupadas$contextType < 3,]
+
+ggplot(porContext, aes(x=indTarget,y=mean,col=contextType)) +
+  geom_point(size=2, shape=23)
+
+
+verDiff <-  data.frame(dcast(porContext, 
+                      formula = indTarget~contextType,
+                      fun.aggregate = sum,value.var = "mean"))
+
+verDiff["diff"] <- abs( verDiff["X1"] - verDiff["X2"])
+
+umbralDiff = .6
+umbralAcc = .3
+verDiff$umbral =  verDiff["diff"] > umbralDiff |
+                  verDiff["X1"]   < umbralAcc | 
+                  verDiff["X2"]   < umbralAcc
+
+ggplot(verDiff, aes(x=indTarget,y=diff,col=umbral)) +
+  geom_point(size=2, shape=23) + 
+  geom_hline(yintercept = umbral)
+
+verDiff[verDiff["umbral"]==TRUE,]
+
