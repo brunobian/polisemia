53a9f6e82828bbd55be076e1390c962df0de0e9c
bbianchi <brunobian@gmail.com>
2024-05-10 17:46:46 -0300

    wordlevel model added to sesgo_LLM

--- LLM/plot_sesgos.R
+++ LLM/plot_sesgos.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 setwd("/data/brunobian/Documents/Repos/Repos_Analisis/polisemia/LLM/")
-df=read.csv2("distancias.csv",sep=",")
+df=read.csv("distancias_nuevo_modelo.csv",sep=",")
 
 df[,"baseS1"] <- as.numeric(as.character(df[,"X2"]))
 df[,"SignS1"] <- as.numeric(as.character(df[,"X3"]))
@@ -14,6 +14,7 @@ ggplot(df) +
   xlab("Sesgo Base") +                                                          
   ylab("Sesgo Generado")
 
+ggsave("base_generado.png", plot = p)
 df["diff_base1_emb"] <- (df["SignS1"] - df["baseS1"]) / abs(df["baseS1"])
 df["diff_base2_emb"] <- (df["SignS2"] - df["baseS2"]) / abs(df["baseS2"])
 
@@ -25,7 +26,7 @@ df2[,"diff_base2"] <- as.numeric(as.character(df2[,"diff_base2"]))
 
 df <- merge(df,df2,by.x="X0",by.y="indTarget")
 
-ggplot(df) +                                                               
+ ggplot(df) +                                                               
   geom_point(aes(x=diff_base1,y=diff_base1_emb), col="#00CDCD")+                           
   geom_point(aes(x=diff_base2,y=diff_base2_emb), col= "#00CDCD")+                          
   geom_abline(linetype="longdash") +
@@ -36,3 +37,4 @@ ggplot(df) +
   xlim(c(-0.1,0.5))+
   ylim(c(-1,2))
 
+ggsave("humano_embedding.png", plot = q)
